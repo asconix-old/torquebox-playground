0f3020114f88c496fc76aaa91bf3e932835c53ea
Christoph Pilka <c.pilka@massive-insights.com>
2013-06-08 01:18:55 +0200

    Changed the concurrency to 1.

--- config/torquebox.rb
+++ config/torquebox.rb
@@ -13,7 +13,7 @@ TorqueBox.configure do
   queue "/queues/torquebox-playground" do
     durable false
     processor App::MessageProcessors::HpPublisher do
-      concurrency 100
+      concurrency 1
     end
   end
 
